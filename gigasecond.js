89f5df36aee4c087f1dd7794790035b245a1b9a8
KN Tse <overload0715@gmail.com>
2023-06-29 22:59:25 +0800

    Update ex

--- gigasecond.js
+++ gigasecond.js
@@ -1,19 +1,20 @@
-const parseISO = require('date-fns/parseISO')
-const add = require('date-fns/add')
-const format = require('date-fns/format')
-const prompt = require('prompt-sync')();
+const parseISO = require("date-fns/parseISO");
+const add = require("date-fns/add");
+const format = require("date-fns/format");
+const prompt = require("prompt-sync")();
 
 function gigasecond() {
+  const dateString = prompt(`What is the date : `);
 
-    const dateString = prompt(`What is the date : `)
+  if (dateString !== "") {
+    date = parseISO(dateString);
+  } else date = new Date();
 
-    if (dateString !== "") {
-         date = parseISO(dateString)
-    }else date = new Date();
-
-    console.log("The date is :" + format(date, 'yyyy-MM-dd iii kk:mm:ss OOOO'))
-    console.log("The date after GigaSecond is :" + format(add(date, {seconds: 1000000000}), 'yyyy-MM-dd iii kk:mm:ss OOOO'))
-
+  console.log("The date is :" + format(date, "yyyy-MM-dd iii kk:mm:ss OOOO"));
+  console.log(
+    "The date after GigaSecond is :" +
+      format(add(date, { seconds: 1000000000 }), "yyyy-MM-dd iii kk:mm:ss OOOO")
+  );
 }
 
-gigasecond()
+gigasecond();
