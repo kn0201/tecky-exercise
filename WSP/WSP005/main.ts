e2d1f05320a1961c4cae9ef82179503dda9ee62e
KN Tse <overload0715@gmail.com>
2023-07-25 10:25:52 +0800

    adding POST method

--- WSP/WSP005/main.ts
+++ WSP/WSP005/main.ts
@@ -1,18 +1,16 @@
 import express from "express";
-// import { Request, Response } from "express";
+// import { Request, Response } from 'express'
 import { print } from "listening-on";
 import expressSession from "express-session";
 import crypto from "crypto";
 import dayjs from "dayjs";
 import path from "path";
+import formidable from "formidable";
+import { mkdirSync } from "fs";
+import jsonfile from "jsonfile";
 
 const app = express();
-
-// app.use(express.static("public"));
-// app.listen(8080, () => {
-//   console.log("Ready");
-//   console.log(`http://localhost:8080`);
-// });
+app.use(express.urlencoded());
 
 app.use(
   expressSession({
@@ -42,26 +40,94 @@ app.get("/", (req, res) => {
 });
 
 app.use("/public", express.static("public"));
+app.use("/uploads", express.static("uploads"));
 
-app.use((req, res) => {
-  // res.redirect('/public/404.html')
-  res.sendFile(path.resolve("public", "404.html"));
+let uploadDir = "uploads";
+mkdirSync(uploadDir, { recursive: true });
+
+let memos = [
+  { content: "網上連儂牆" },
+  { content: "香港加油" },
+  { content: "yukimin", filename: "ebe1b6c4-08a3-4b2b-9058-d2c58ca9ee80.jpeg" },
+];
+
+try {
+  memos = jsonfile.readFileSync("memos.json");
+} catch (error) {}
+
+let contact = [{ id: 0, email: "admin@admin", password: "admin" }];
+
+try {
+  contact = jsonfile.readFileSync("contact.json");
+} catch (error) {}
+
+app.get("/memos.js", (req, res) => {
+  res.end(`let memos = ${JSON.stringify(memos)}`);
 });
 
-type Memo = {
-  // id: number;
-  content: string;
-  postTime: number;
-};
-
-let memos: Memo[] = [];
-
-app.get("/memos", (req, res) => {
-  res.json(memos);
+app.get("/contact.js", (req, res) => {
+  res.end(`let contact = ${JSON.stringify(contact)}`);
+});
+let i = 1;
+app.post("/contact", async (req, res) => {
+  console.log("saving contact: ", req.body);
+  contact.push({
+    id: i,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  await jsonfile.writeFile("contact.json", contact);
+  res.redirect("/");
 });
 
 app.post("/memos", (req, res) => {
-  memos.push({ content: "?", postTime: Date.now() });
+  let form = formidable({
+    uploadDir,
+    maxFileSize: 300 * 1024,
+    allowEmptyFiles: false,
+    filter(part) {
+      return part.mimetype?.startsWith("image/") || false;
+    },
+    filename(name, ext, part, form) {
+      return crypto.randomUUID() + "." + part.mimetype?.split("/").pop();
+    },
+  });
+  form.parse(req, async (err, fields, files) => {
+    if (err) {
+      res.status(400);
+      res.json({ error: String(err) });
+      return;
+    }
+    let image = toArray(files.image)[0];
+    let filename = image?.newFilename;
+
+    let content = toArray(fields.content)[0];
+    if (!content) {
+      res.json({ error: "missing content" });
+      res.status(400);
+      return;
+    }
+
+    memos.push({
+      content,
+      filename,
+    });
+
+    await jsonfile.writeFile("memos.json", memos);
+
+    // res.json({ content, filename });
+    res.redirect("/");
+  });
+});
+
+function toArray<T>(field: T[] | T | undefined): T[] {
+  return Array.isArray(field) ? field : field ? [field] : [];
+}
+
+app.use((req, res) => {
+  // res.redirect('/public/404.html')
+  res.status(404);
+  res.sendFile(path.resolve("public", "404.html"));
 });
 
 app.listen(8080, () => {
