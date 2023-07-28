224ebcfdf957a0d991dcb99bb7cfd3b11f815b41
KN Tse <overload0715@gmail.com>
2023-07-28 09:20:52 +0800

    change name

--- WSP/WSP009/main.ts
+++ WSP/WSP009/main.ts
@@ -1,30 +1,84 @@
-import express from "express";
-import { print } from "listening-on";
 import { Client } from "pg";
-import dotenv from "dotenv";
+import { env } from "./env";
+import XLSX from "xlsx";
+import "cast.ts";
+import { object, optional, string } from "cast.ts";
 
-dotenv.config();
+let file = "./WSP009-exercise.xlsx";
+let workbook = XLSX.readFile(file);
 
-export const client = new Client({
-  database: process.env.DB_NAME,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-});
-client.connect();
+let userSheet = workbook.Sheets.user;
+let userArray = XLSX.utils.sheet_to_json(userSheet);
 
-const app = express();
-app.use(express.urlencoded({ extended: false }));
-
-app.get("/", (req, res) => {
-  res.redirect("/public");
+let userParser = object({
+  username: string(),
+  password: string(),
 });
 
-app.use("/public", express.static("public"));
-app.use("/uploads", express.static("uploads"));
+let memoSheet = workbook.Sheets.memo;
+let memoArray = XLSX.utils.sheet_to_json(memoSheet);
 
-app.listen(8080, () => {
-  // console.log('ready')
-  // console.log('listening on port 8080')
-  // console.log('you can connect to this server at http://localhost:8080')
-  print(8080);
+let memoParser = object({
+  content: string(),
+  image: optional(string()),
 });
+
+async function main() {
+  const client = new Client({
+    database: env.DB_NAME,
+    user: env.DB_USERNAME,
+    password: env.DB_PASSWORD,
+  });
+
+  await client.connect();
+
+  for (let object of userArray) {
+    let user = userParser.parse(object);
+
+    let result = await client.query(
+      /* sql */ `
+      select id
+      from "users"
+      where username = $1
+    `,
+      [user.username]
+    );
+
+    if (result.rowCount == 0) {
+      await client.query(
+        /* sql */ `
+  insert into "users" (username, password) values ($1,$2)
+  `,
+        [user.username, user.password]
+      );
+    } else if (result.rowCount == 1) {
+      await client.query(
+        /* sql */ `
+        update "users"
+        set password = $1
+        where id = $2
+      `,
+        [user.password, result.rows[0].id]
+      );
+    } else {
+      console.log("duplicated users:", result.rows);
+      throw new Error("duplicated user");
+    }
+  }
+
+  await client.query(/* sql */ `
+    delete from "memos"
+  `);
+  for (let object of memoArray) {
+    let memo = memoParser.parse(object);
+    await client.query(
+      /* sql */ `
+  insert into "memos" (content, image) values ($1,$2)
+  `,
+      [memo.content, memo.image]
+    );
+  }
+
+  await client.end();
+}
+main().catch((e) => console.error(e));
